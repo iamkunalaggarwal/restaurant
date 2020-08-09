1c9d5fe946e01ac9ea8c5d7d68848e0ad98581b1
kunal <krkunalaggarwal@gmail.com>
2020-08-09 16:03:07 +0530

    forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Dishdetail from './DishDetailComponent';
 import Header from './headercomponent';
 import Footer from './footerComponent';
 import Contact from './contactComponent';
+import About from './aboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -55,6 +56,7 @@ class Main extends Component {
         <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>}/>
         <Route path="/menu/:dishId" component={DishWithId}/>
         <Route exact path='/contactus' component={Contact}/>
+        <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders}/>}/>
         <Redirect to="/home"/>
       </Switch>
       <Footer />
