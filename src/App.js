44d30ec103fcbe3d18dac8d880eeaf94a5fbfc2d
kunal <krkunalaggarwal@gmail.com>
2020-09-01 10:43:22 +0530

    'final'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import Main  from './components/MainComponent';
-import {BrowserRouter} from 'react-router-dom';
-import './App.css';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu  from './components/menuComponents';
 
+import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
   render(){
   return (
-    <BrowserRouter>
     <div>
-      <Main/>
+      <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+      </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes}/>
     </div>
-    </BrowserRouter>
   );
 }
 }
--- src/components/menuComponents.js
+++ src/components/menuComponents.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Media } from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
-    function RenderMenuItem({dish}){
+class Menu extends Component{
 
-        return(
-            <Card> 
-                <Link to={`/menu/${dish.id}`}>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-                </Link>
-            </Card>
 
-        );
+    constructor(props){
+        super(props);
+
+        this.state={
+
+            selectedDish: null
+
+
+        }
     }
 
-    const Menu =(props) =>{
+    onDishSelect(dish){
 
-        const menu  = props.dishes.map((dish) => {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish){
+        if(dish!=null){
+
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+
+            );
+
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render(){
+
+        const menu  = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <RenderMenuItem dish={dish}/>
+                <div key={dish.id} className="col-12 col-md-5 mt-1">
+                    <Card onClick={() => this.onDishSelect(dish)}> 
+                            <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
 
@@ -32,21 +63,16 @@ import {Link} from 'react-router-dom';
         return(
             <div className="container">
                 <div className="row">
-                    <Breadcrumb>
-                    <BreadcrumbItem><Link to='/home'>Home</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Menu</h3>
-                    </div>
-                </div>
-                <div className="row">
                         {menu}
 
+                </div>
+                <div className="row" >
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
 
         );
     }
+}
+
 export default Menu;
